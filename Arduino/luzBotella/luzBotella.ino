58c089b874bc86acd4069fd444b257bb0e4b77f6
itpstudent <itpstudent@10-17-53-144.dynapool.wireless.nyu.edu>
2019-07-10 16:19:59 -0400

    Code to change 44 RGB neopixel strand from whole to spotlight with pushbotton

---
+++ Arduino/luzBotella/luzBotella.ino
@@ -0,0 +1,81 @@
+// This is a demonstration on how to use an input device to trigger changes on your neo pixels.
+// You should wire a momentary push button to connect from ground to a digital IO pin.  When you
+// press the button it will change to a new pixel animation.  Note that you need to press the
+// button once to start the first animation!
+
+#include <Adafruit_NeoPixel.h>
+
+#define BUTTON_PIN   13    // Digital IO pin connected to the button.  This will be
+                          // driven with a pull-up resistor so the switch should
+                          // pull the pin to ground momentarily.  On a high -> low
+                          // transition the button press logic will execute.
+
+#define PIXEL_PIN    6    // Digital IO pin connected to the NeoPixels.
+
+#define PIXEL_COUNT 44
+
+// Parameter 1 = number of pixels in strip,  neopixel stick has 8
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_RGB     Pixels are wired for RGB bitstream
+//   NEO_GRB     Pixels are wired for GRB bitstream, correct for neopixel stick
+//   NEO_KHZ400  400 KHz bitstream (e.g. FLORA pixels)
+//   NEO_KHZ800  800 KHz bitstream (e.g. High Density LED strip), correct for neopixel stick
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+
+bool oldState = HIGH;
+int showType = 0;
+uint32_t white = strip.Color(127, 127, 127);
+
+void setup() {
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  strip.begin();
+  strip.fill(white,0,0);
+  strip.show(); // Initialize all pixels to 'off'
+}
+
+void loop() {
+  // Get current button state.
+  bool newState = digitalRead(BUTTON_PIN);
+
+  // Check if state changed from high to low (button press).
+  if (newState == LOW && oldState == HIGH) {
+    // Short delay to debounce button.
+    delay(20);
+    // Check if button is still low after debounce.
+    newState = digitalRead(BUTTON_PIN);
+    if (newState == LOW) {
+      showType++;
+      if (showType > 1)
+        showType=0;
+      startShow(showType);
+    }
+  }
+
+  // Set the last button state to the old state.
+  oldState = newState;
+}
+
+void startShow(int i) {
+  switch(i){
+    case 0: todo(200);    // Black/off
+            break;
+    case 1:  mitad(200); // Blue
+            break;
+  }
+}
+
+// Fill the dots one after the other with a color
+void mitad( uint8_t wait){
+  strip.clear();
+  strip.show();
+  strip.fill(white, 14, 16);
+  strip.show();
+  delay(wait);
+}
+
+void todo(uint8_t wait) {
+    strip.fill(white,0,0);
+    strip.show();
+    delay(wait);
+}
